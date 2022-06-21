0ed5e348588714f5ed38212fc6b775e028a3436f
Thierry <varelathierry@gmail.com>
2022-06-21 18:14:20 -0300

    exercicio 1 tema variaveis js

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/exercicio-01.js
@@ -0,0 +1,25 @@
+// 1. Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
+const myName = "Thierry";
+
+// 2. Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+const birthCity = "Curitiba";
+
+// 3. Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
+let birthYear = 1998;
+
+// 4. Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+console.log(myName)
+console.log(birthCity)
+console.log(birthYear)
+
+// 5. Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+birthYear = 2030;
+console.log(birthYear);
+
+// 6. Altere o valor atribuído à constante birthCity. Faça um console.log(birthCity) novamente! 
+// Você saberia explicar por que recebemos uma mensagem de erro? 🤔
+
+birthCity = "São Paulo";
+console.log(birthCity)
+
+// A mensagem de erro aparece pois não é possível alterar o valor atribuído a uma constante.
