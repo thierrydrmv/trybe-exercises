7c3b2c978bd0cf745b90d3b3b1c6e44a0346f4a6
Thierry <varelathierry@gmail.com>
2022-06-22 19:23:50 -0300

    exercicio utilizando for

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-for/exercicios/exercicio-07.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-for/exercicios/exercicio-07.js
@@ -1,6 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     let menorNumero;
-for (var index=0; index<numbers.length; index+=1) {
+for (let index=0; index<numbers.length; index+=1) {
     if (numbers[index] < menorNumero || menorNumero == undefined) {
         menorNumero = numbers[index]
     }
