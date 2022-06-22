dde6c0f2237f2cce16c9bc8a0c32d55c1ca6b474
Thierry <varelathierry@gmail.com>
2022-06-22 10:44:59 -0300

    debugging code

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/exercicio-05.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/exercicio-05.js
@@ -5,12 +5,13 @@ const c = 60;
 let triangulo = a + b + c
 
 if (triangulo == 180) {
-    console.log("true")
+    triangulo = true;
 }
 else if (a < 0 || b < 0 || c < 0) {
     console.log("Ângulos inválidos")
 }
 
 else {
-    console.log("False")
+    triangulo = false;
 }
+console.log(triangulo)
