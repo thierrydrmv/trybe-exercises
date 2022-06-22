04ddd2927fb2f31232133423d6d2f6c3cfdee587
Thierry <varelathierry@gmail.com>
2022-06-21 23:34:08 -0300

    exercicio 1, operadores aritméticos

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/exercicio-01.js
@@ -0,0 +1,17 @@
+const a = 7;
+const b = 5;
+
+let soma = a + b;
+console.log(soma)
+
+let subtracao = a - b;
+console.log(subtracao)
+
+let multiplicacao = a * b;
+console.log(multiplicacao)
+
+let divisao = a / b;
+console.log(divisao)
+
+let modulo = a % b;
+console.log(modulo)
