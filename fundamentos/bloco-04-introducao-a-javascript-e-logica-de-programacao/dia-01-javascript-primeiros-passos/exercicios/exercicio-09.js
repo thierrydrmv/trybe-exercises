43527923a634f5608341ea77ffa6beae17d6e409
Thierry <varelathierry@gmail.com>
2022-06-22 11:03:18 -0300

    exercicio sobre módulo para calcular se é impar

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/exercicio-09.js
@@ -0,0 +1,13 @@
+const num1 = 10;
+const num2 = 11;
+const num3 = 12;
+
+let impar;
+
+if (num1 % 2 != 0 || num2 % 2 != 0 || num3 % 2 != 0) {
+    impar = true;
+}
+else {
+    impar = false;
+}
+console.log(impar)
