9cbf8d252d093d735fcbf9a6c1195c4c6d21755d
Thierry <varelathierry@gmail.com>
2022-06-22 10:59:48 -0300

    exercicio sobre módulo para calcular se é par

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/exercicio-08.js
@@ -0,0 +1,13 @@
+const num1 = 1;
+const num2 = 3;
+const num3 = 8;
+
+let par;
+
+if(num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0) {
+    par = true;
+}
+else {
+    par = false;
+}
+console.log(par)
