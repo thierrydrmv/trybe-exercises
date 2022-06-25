6568a80c410a5763ec565b37552ef6f537d605f2
Thierry <varelathierry@gmail.com>
2022-06-25 13:06:08 -0300

    exercicio bonus quadrado

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-e-algoritmos/bonus/exercicio-01.js
@@ -0,0 +1,11 @@
+let n = 5;
+let asterisco = "";
+
+for  (let i=0; i<n; i+=1) {
+    if(n>1) {
+        asterisco += "*"
+    }
+}
+for  (let i=0; i<n; i+=1) {
+console.log(asterisco)
+}
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-e-algoritmos/bonus/exercicio-02.js
@@ -0,0 +1,9 @@
+let n = 5;
+let asterisco = "";
+
+for  (let i=0; i<n; i+=1) {
+    if(n>1) {
+        asterisco += "*"
+    }
+    console.log(asterisco)
+}
