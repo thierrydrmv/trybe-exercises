f84c6db397348b14362578ffee1cae8196d08055
Thierry <varelathierry@gmail.com>
2022-07-14 18:21:55 -0300

    exercicio 1 utilizando arrow function, template literals, ternary operator

---
+++ fundamentos/bloco-07-introducao-es6-e-testes-unitarios/exercicios/parte-1/exercicio1.js
@@ -0,0 +1,17 @@
+const testingScope = (escopo) => (escopo === true) ? console.log(`Não devo ser utilizada fora do meu escopo (if) 
+ótimo, fui utilizada no escopo !`) : console.log(`Não devo ser utilizada fora meu escopo (else)`);
+    
+// function testingScope(escopo) {
+//     if (escopo === true) {
+//       var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//       console.log(ifScope);
+//     } else {
+//       var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//     console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+//   }
+
+  testingScope(true);
+  testingScope(false);
