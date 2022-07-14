b3d12728cd3d211195f7ee018f30825136688ba9
Thierry <varelathierry@gmail.com>
2022-07-14 18:40:23 -0300

    resolvendo como o exercício pede

--- fundamentos/bloco-07-introducao-es6-e-testes-unitarios/exercicios/parte-1/exercicio1.js
+++ fundamentos/bloco-07-introducao-es6-e-testes-unitarios/exercicios/parte-1/exercicio1.js
@@ -1,17 +1,16 @@
-const testingScope = (escopo) => (escopo === true) ? console.log(`Não devo ser utilizada fora do meu escopo (if) 
-ótimo, fui utilizada no escopo !`) : console.log(`Não devo ser utilizada fora meu escopo (else)`);
+// const testingScope = (escopo) => (escopo === true) ? console.log(`Não devo ser utilizada fora do meu escopo (if) 
+// ótimo, fui utilizada no escopo !`) : console.log(`Não devo ser utilizada fora meu escopo (else)`);
     
-// function testingScope(escopo) {
-//     if (escopo === true) {
-//       var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-//       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-//       console.log(ifScope);
-//     } else {
-//       var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-//       console.log(elseScope);
-//     }
-//     console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-//   }
+const testingScope = (escopo) => {
+    if (escopo === true) {
+      let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+      console.log(ifScope);
+    } else {
+      let elseScope = `Não devo ser utilizada fora meu escopo (else)`;
+      console.log(elseScope);
+    }
+  }
 
   testingScope(true);
   testingScope(false);
