7a331970bf8d3c95e31b74d4236daf9082b959cb
Thierry <varelathierry@gmail.com>
2022-07-26 20:37:29 -0300

    exercicio 1 reduce e concat

---
+++ fundamentos/bloco-08-introducao-a-higher-order-functions/dia-04-hofs-reduce/exercicio1.js
@@ -0,0 +1,9 @@
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+  ];
+  
+  const flatten = (arrays) => arrays.reduce((acc, curr) => acc.concat(curr), []);
+
+  console.log(flatten(arrays));
