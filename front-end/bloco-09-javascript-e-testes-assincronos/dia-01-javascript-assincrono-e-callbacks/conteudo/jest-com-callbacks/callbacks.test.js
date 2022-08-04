d87feb9f93be3b5cc2f4dd2b5bb5c9016b796445
Thierry <varelathierry@gmail.com>
2022-08-04 20:22:19 -0300

    exercicio 2 callbacks com done

--- front-end/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/conteudo/jest-com-callbacks/callbacks.test.js
+++ front-end/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/conteudo/jest-com-callbacks/callbacks.test.js
@@ -1,8 +1,9 @@
 test('Não deveria passar!', (done) => {
 		setTimeout(() => {
 		try {
-			expect(10).toBe(5);
+			expect(10).toBe(10);
 			console.log('Deveria falhar!');
+			done();
 		} catch (error) {
 			done(error);
 		}
