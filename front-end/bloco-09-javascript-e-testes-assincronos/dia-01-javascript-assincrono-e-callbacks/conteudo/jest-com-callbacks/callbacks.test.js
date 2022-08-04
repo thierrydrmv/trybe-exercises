0a1ec8af44a706247ec806ab9cccd4ca3dcb0497
Thierry <varelathierry@gmail.com>
2022-08-04 20:18:30 -0300

    exercicio test com timeout

---
+++ front-end/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/conteudo/jest-com-callbacks/callbacks.test.js
@@ -0,0 +1,10 @@
+test('Não deveria passar!', (done) => {
+		setTimeout(() => {
+		try {
+			expect(10).toBe(5);
+			console.log('Deveria falhar!');
+		} catch (error) {
+			done(error);
+		}
+		}, 500);
+	});
