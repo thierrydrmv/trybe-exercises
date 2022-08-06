37f66f852c5e5850e74153627861e5c7d02106a5
Thierry <varelathierry@gmail.com>
2022-08-05 22:04:31 -0300

    exercicio 8 chamando a função assincrona

---
+++ front-end/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercicios/exercise8.js
@@ -0,0 +1,41 @@
+const pokemons = [
+  {
+    name: 'Bulbasaur',
+    type: 'Grass',
+    ability: 'Razor Leaf',
+  },
+  {
+    name: 'Charmander',
+    type: 'Fire',
+    ability: 'Ember',
+  },
+  {
+    name: 'Squirtle',
+    type: 'Water',
+    ability: 'Water Gun',
+  },
+];
+
+function getPokemonDetails(filter, callback) {
+  setTimeout(() => {
+    if (pokemons.find(filter) === undefined) {
+      return callback(new Error('Não temos esse pokémon para você :('));
+    }
+    const pokemon = pokemons.find(filter);
+
+    const { name, type, ability } = pokemon;
+
+    const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
+
+    callback(messageFromProfOak);
+  }, 2000);
+}
+//Complete a chamada da função getPokemonDetails de modo que ela imprima no 
+//console os detalhes do pokémon que você escolheu.
+
+
+getPokemonDetails(element => element.name === 'Charman1der', console.log);
+
+module.exports = {
+  getPokemonDetails,
+};
