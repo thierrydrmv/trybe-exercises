4da7668816dcb57d6a5a62c0e76c3ec698af14b5
Thierry <varelathierry@gmail.com>
2022-08-17 18:56:09 -0300

    alternative text

--- front-end/bloco-10-introducao-a-react/dia-02-componentes-react/conteudo/fixation-exercises-10-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-02-componentes-react/conteudo/fixation-exercises-10-2/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText= 'cat'/>
+        <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText= 'Cute cat staring,'/>
         <p>
           How did the Mom Cat know she was pregnant? Her test was pawsitive.
         </p>
