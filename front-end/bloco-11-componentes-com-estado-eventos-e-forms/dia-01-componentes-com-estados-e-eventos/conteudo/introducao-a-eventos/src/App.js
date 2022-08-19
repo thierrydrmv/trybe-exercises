5f7f896e13146dc597aa2798d750cb632663ece9
Thierry <varelathierry@gmail.com>
2022-08-19 15:44:03 -0300

    adicionando as funções a classe e trazendo o this para dentro das funções utilizando arrow function

--- front-end/bloco-11-componentes-com-estado-eventos-e-forms/dia-01-componentes-com-estados-e-eventos/conteudo/introducao-a-eventos/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-forms/dia-01-componentes-com-estados-e-eventos/conteudo/introducao-a-eventos/src/App.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react'
 
-function food() {
-  console.log('Pizza')
-}
-
-function drink() {
-  console.log('Coca-cola')
-}
-
-function watch() {
-  console.log('Filme')
-}
-
 export default class App extends Component {
+
+  food = () => {
+    console.log(this);
+    console.log('Pizza')
+  }
+  
+  drink = () => {
+    console.log(this);
+    console.log('Coca-cola')
+  }
+  
+  watch = () => {
+    console.log(this);
+    console.log('Filme')
+  }
+
   render() {
     return (
       <>
-        <button onClick={food}>Hamburguer ou pizza?</button>
-        <button onClick={drink}>Guaraná ou Coca-cola?</button>
-        <button onClick={watch}>Filme ou Série?</button>
+        <button onClick={this.food}>Hamburguer ou pizza?</button>
+        <button onClick={this.drink}>Guaraná ou Coca-cola?</button>
+        <button onClick={this.watch}>Filme ou Série?</button>
       </>
     )
   }
