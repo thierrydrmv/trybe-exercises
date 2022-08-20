30782275cd2d6360cf88767e40dfbe5556a61558
Thierry <varelathierry@gmail.com>
2022-08-20 17:15:15 -0300

    criando um contador em cada botao usando state

--- front-end/bloco-11-componentes-com-estado-eventos-e-forms/dia-01-componentes-com-estados-e-eventos/conteudo/introducao-a-eventos/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-forms/dia-01-componentes-com-estados-e-eventos/conteudo/introducao-a-eventos/src/App.js
@@ -2,17 +2,29 @@ import React, { Component } from 'react'
 
 export default class App extends Component {
 
+  state = {
+    clickFood: 0,
+    clickDrink: 0,
+    clickWatch: 0
+  }
+
   food = () => {
+    this.setState((before, _props) => ({
+      clickFood:before.clickFood + 1}))
     console.log(this);
     console.log('Pizza')
   }
   
   drink = () => {
+    this.setState((before, _props) => ({
+      clickDrink:before.clickDrink + 1}))
     console.log(this);
     console.log('Coca-cola')
   }
   
   watch = () => {
+    this.setState((before, _props) => ({
+      clickWatch:before.clickWatch + 1}))
     console.log(this);
     console.log('Filme')
   }
