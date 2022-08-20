f4c7a4b9901587591708721124b213daa7226baf
Thierry <varelathierry@gmail.com>
2022-08-20 18:38:00 -0300

    trocando a cor dos botões usando o style{{}}

--- front-end/bloco-11-componentes-com-estado-eventos-e-forms/dia-01-componentes-com-estados-e-eventos/conteudo/introducao-a-eventos/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-forms/dia-01-componentes-com-estados-e-eventos/conteudo/introducao-a-eventos/src/App.js
@@ -10,31 +10,42 @@ export default class App extends Component {
 
   food = () => {
     this.setState((before, _props) => ({
-      clickFood:before.clickFood + 1}))
+      clickFood:before.clickFood + 1}), () => {
+        console.log(this.color(this.state.clickFood))
+      })
     console.log(this);
     console.log('Pizza')
   }
   
   drink = () => {
     this.setState((before, _props) => ({
-      clickDrink:before.clickDrink + 1}))
+      clickDrink:before.clickDrink + 1}), () => {
+        console.log(this.color(this.state.clickDrink))
+      })
     console.log(this);
     console.log('Coca-cola')
   }
   
   watch = () => {
     this.setState((before, _props) => ({
-      clickWatch:before.clickWatch + 1}))
+      clickWatch:before.clickWatch + 1}), () => {
+        console.log(this.color(this.state.clickWatch))
+      })
     console.log(this);
     console.log('Filme')
   }
 
+  color = (value) => {
+    return (value%2===0) ? 'green' : 'red';
+  }
+
   render() {
+    const {clickDrink, clickFood, clickWatch} = this.state;
     return (
       <>
-        <button onClick={this.food}>Hamburguer ou pizza?</button>
-        <button onClick={this.drink}>Guaraná ou Coca-cola?</button>
-        <button onClick={this.watch}>Filme ou Série?</button>
+        <button style={{backgroundColor: this.color(clickFood)}} onClick={this.food}>Hamburguer ou pizza?</button>
+        <button style={{backgroundColor: this.color(clickDrink)}} onClick={this.drink}>Guaraná ou Coca-cola?</button>
+        <button style={{backgroundColor: this.color(clickWatch)}} onClick={this.watch}>Filme ou Série?</button>
       </>
     )
   }
