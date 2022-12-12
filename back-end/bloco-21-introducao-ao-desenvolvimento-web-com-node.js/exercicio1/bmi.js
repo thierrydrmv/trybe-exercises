83bb07e30b574f474f231937d551abb6e3793c63
Thierry <varelathierry@gmail.com>
2022-12-12 00:01:28 -0300

    mensagem de texto do imc

--- back-end/bloco-21-introducao-ao-desenvolvimento-web-com-node.js/exercicio1/bmi.js
+++ back-end/bloco-21-introducao-ao-desenvolvimento-web-com-node.js/exercicio1/bmi.js
@@ -7,10 +7,26 @@ const imcWithHeightInMeters = (weight, height) => {
 
 const main = () => {
   const peso = readline.question('Qual seu peso? ');
-  const altura = readline.question('Qual sua altura? ');
-  const resultado = imcWithHeightInMeters(peso, altura)
+  const altura = readline.question('Qual sua altura (cm)? ');
+  const resultado = imcWithHeightInMeters(peso, altura/100)
 
   console.log(`Seu imc é ${resultado.toFixed(2)}!`);
+
+  switch(true) {
+    case(resultado < 18.5):
+    return console.log('Abaixo do peso (magreza)');
+    case(resultado > 18.5 && resultado < 25):
+    return console.log('Peso normal');
+    case(resultado > 25 && resultado < 30):
+    return console.log('Acima do peso (sobrepeso)');
+    case(resultado > 30 && resultado < 35):
+    return console.log('Obesidade grau I');
+    case(resultado > 35 && resultado < 40):
+    return console.log('Obesidade grau II');
+    default:
+      return console.log('Obesidade graus III e IV');
+  }
+
 }
 
 main();
