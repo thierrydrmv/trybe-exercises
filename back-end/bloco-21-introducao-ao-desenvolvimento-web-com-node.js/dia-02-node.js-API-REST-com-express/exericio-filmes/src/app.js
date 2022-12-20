a3e85f03678ca30a61faf32429f7b5734a9f4394
Thierry <varelathierry@gmail.com>
2022-12-20 17:10:36 -0300

    utilizando método get para trazer todos os dados

--- back-end/bloco-21-introducao-ao-desenvolvimento-web-com-node.js/dia-02-node.js-API-REST-com-express/exericio-filmes/src/app.js
+++ back-end/bloco-21-introducao-ao-desenvolvimento-web-com-node.js/dia-02-node.js-API-REST-com-express/exericio-filmes/src/app.js
@@ -1,3 +1,12 @@
 const express = require('express');
+const movies = require('./utils/readMovies');
 
 const app = express();
+
+app.use(express.json());
+
+app.get('/movies', async (req, res) => {
+  return res.status(200).send(await movies());
+})
+
+module.exports = app
