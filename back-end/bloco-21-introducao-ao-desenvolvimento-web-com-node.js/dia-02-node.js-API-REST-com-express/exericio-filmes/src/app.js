b0b03620d524576cc25e7aea3507700f75f5e810
Thierry <varelathierry@gmail.com>
2022-12-20 18:36:35 -0300

    adicionando filmes a lista

--- back-end/bloco-21-introducao-ao-desenvolvimento-web-com-node.js/dia-02-node.js-API-REST-com-express/exericio-filmes/src/app.js
+++ back-end/bloco-21-introducao-ao-desenvolvimento-web-com-node.js/dia-02-node.js-API-REST-com-express/exericio-filmes/src/app.js
@@ -1,19 +1,26 @@
 const express = require('express');
-const movies = require('./utils/readMovies');
+const readMovies = require('./utils/readMovies');
+const addMovies = require('./utils/addNewMovie');
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/movies', async (req, res) => {
-  return res.status(200).send(await movies());
-})
+  return res.status(200).send(await readMovies());
+});
 
 app.get('/movies/:id', async(req, res) => {
   const { id } = req.params;
-  const allMovies = await movies();
+  const allMovies = await readMovies();
   const movie = allMovies.filter((m) => m.id === Number(id));
   return res.status(200).send(movie);
-})
+});
+
+app.post('/movies', async(req, res) => {
+  const bodyData = req.body;
+  addMovies(bodyData);
+  return res.status(201).send(bodyData);
+});
 
 module.exports = app
