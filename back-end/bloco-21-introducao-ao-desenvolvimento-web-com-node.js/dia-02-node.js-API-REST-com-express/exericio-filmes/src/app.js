b86bf2e099d764af573c2658443d7e703f4379b8
Thierry <varelathierry@gmail.com>
2022-12-20 23:10:36 -0300

    exercicio metodo delete

--- back-end/bloco-21-introducao-ao-desenvolvimento-web-com-node.js/dia-02-node.js-API-REST-com-express/exericio-filmes/src/app.js
+++ back-end/bloco-21-introducao-ao-desenvolvimento-web-com-node.js/dia-02-node.js-API-REST-com-express/exericio-filmes/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const readMovies = require('./utils/readMovies');
 const addMovies = require('./utils/addNewMovie');
 const editMovies = require('./utils/editMovies');
+const deleteMovie = require('./utils/deleteMovie');
 
 const app = express();
 
@@ -31,6 +32,12 @@ app.put('/movies/:id', async(req, res) => {
   if (!allMovies.find((m) => m.id === Number(id))) return res.status(404).send({message: 'Movie Not Found'})
   await editMovies(id, bodyData)
   return res.status(200).send(allMovies)
+})
+
+app.delete('/movies/:id', async(req, res) => {
+  const { id } = req.params;
+  deleteMovie(id)
+  return res.status(200).end();
 })
 
 module.exports = app
--- back-end/bloco-21-introducao-ao-desenvolvimento-web-com-node.js/dia-02-node.js-API-REST-com-express/exericio-filmes/src/utils/editMovies.js
+++ back-end/bloco-21-introducao-ao-desenvolvimento-web-com-node.js/dia-02-node.js-API-REST-com-express/exericio-filmes/src/utils/editMovies.js
@@ -7,7 +7,6 @@ const editMovies = async (id, obj) => {
     const moviesList = [...movies];
     const index = moviesList.findIndex((movie) => movie.id === Number(id))
     moviesList[index] = {...movies[index], ...obj}
-    console.log(obj);
     const stringList = JSON.stringify(moviesList);
   try {
     await fs.writeFile(path.resolve(__dirname, '../../movies.json'), stringList)
