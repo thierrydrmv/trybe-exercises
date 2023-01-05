3a18e8a8377a546df249dbdfc0dd924fd75a6071
Thierry <varelathierry@gmail.com>
2023-01-05 11:07:57 -0300

    utilizando barrels nos routers

--- back-end/bloco-21-introducao-ao-desenvolvimento-web-com-node.js/dia-04-node.js-express-e-middlewares/ecoapp/src/routers/activitiesRouter.js
+++ back-end/bloco-21-introducao-ao-desenvolvimento-web-com-node.js/dia-04-node.js-express-e-middlewares/ecoapp/src/routers/activitiesRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { validationName, validationPrice, validationDescription, validationCreateAt, validationRating, validationDifficulty, auth } = require('../middlewares/');
+const { validationName, validationPrice, validationDescription, validationCreateAt, validationRating, validationDifficulty, auth } = require('../middlewares');
 
 const router = express.Router();
 
