493b8f79307c8d0d866776e9712d6c9e4f6dbc9b
Thierry <varelathierry@gmail.com>
2023-01-07 13:14:29 -0300

    api com express,docker,mysql

--- back-end/bloco-21-introducao-ao-desenvolvimento-web-com-node.js/dia-05-node.js-express-e-mysql/trybecash-api/src/db/peopleDB.js
+++ back-end/bloco-21-introducao-ao-desenvolvimento-web-com-node.js/dia-05-node.js-express-e-mysql/trybecash-api/src/db/peopleDB.js
@@ -6,6 +6,22 @@ const insert = (person) => conn.execute(
     [person.firstName, person.lastName, person.email, person.phone],
 );
 
+const findAll = () => conn.execute('SELECT * FROM people');
+
+const findById = (id) => conn.execute('SELECT * FROM people WHERE id = ?', [id]);
+
+const update = (person, id) => conn.execute(
+  `UPDATE people
+    SET first_name = ?, last_name = ?, email = ?, phone = ? WHERE id = ?`,
+    [person.firstName, person.lastName, person.email, person.phone, id],
+);
+
+const remove = (id) => conn.execute('DELETE FROM people WHERE id = ?', [id]);
+
 module.exports = {
   insert,
+  findAll,
+  findById,
+  update,
+  remove
 };
