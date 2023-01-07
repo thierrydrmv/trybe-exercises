0c4b359fa8be489b40a9c6c020650ac6d5f477da
Thierry de Ridder Martins Varela <varelathierry@gmail.com>
2023-01-07 13:20:16 -0300

    retirando log de user do servidor

--- back-end/bloco-21-introducao-ao-desenvolvimento-web-com-node.js/dia-05-node.js-express-e-mysql/trybecash-api/src/server.js
+++ back-end/bloco-21-introducao-ao-desenvolvimento-web-com-node.js/dia-05-node.js-express-e-mysql/trybecash-api/src/server.js
@@ -7,7 +7,6 @@ const PORT = 3001;
 
 app.listen(PORT, async () => {
   console.log(`Api TrybeCash está sendo executada na porta ${PORT}`);
-  console.log(`Valor da variável de ambiente $USER: ${process.env.USER}`);
   const [result] = await connection.execute('SELECT * FROM people')
   console.log(result);
 })
