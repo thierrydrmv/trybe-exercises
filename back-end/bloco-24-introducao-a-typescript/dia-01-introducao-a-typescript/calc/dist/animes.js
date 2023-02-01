e88800e3c58abf3744a3062b565713f9f2bbd19a
Thierry <varelathierry@gmail.com>
2023-02-01 18:28:03 -0300

    feat: array de objetos anime

---
+++ back-end/bloco-24-introducao-a-typescript/dia-01-introducao-a-typescript/calc/dist/animes.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.animes = void 0;
+exports.animes = [
+    { name: 'Bleach: Sennen Kessen-hen', score: 9.11 },
+    { name: 'Fullmetal Alchemist: Brotherhood', score: 9.11 },
+    { name: 'Steins;Gate', score: 9.08 },
+    { name: 'Gintama', score: 9.07 },
+    { name: 'Kaguya-sama wa Kokurasetai: Ultra Romantic', score: 9.07 },
+    { name: 'Shingeki no Kyojin Season 3 Part 2', score: 9.06 },
+    { name: 'Gintama: The Final', score: 9.05 },
+    { name: 'Gintama', score: 9.04 },
+    { name: 'Hunter x Hunter (2011)', score: 9.04 },
+    { name: 'Ginga Eiyuu Densetsu', score: 9.03 },
+];
+
+console.log(this.animes)
+// reference: https://myanimelist.net/topanime.php
