53a8ad3b67d1ca95537ac0a248a7828c945af12c
Thierry <varelathierry@gmail.com>
2023-04-26 20:02:55 -0300

    feat: ex condicionais e estruturas de repeticao

--- ciencia-da-computacao/bloco-28-introducao-a-python/dia-01-aprendendo-python/conteudo.py
+++ ciencia-da-computacao/bloco-28-introducao-a-python/dia-01-aprendendo-python/conteudo.py
@@ -179,7 +179,7 @@ c = list(range(1, 11, 2))  # saída: [1, 3, 5, 7, 9]
 # podemos utilizar valores negativos para as entradas também
 d = list(range(10, 0, -1))  # saída: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
-print(d)
+# print(d)
 
 
 # Exercício 10
@@ -190,7 +190,7 @@ print(d)
 
 person = ("Thiago", "Nobre", 29)
 
-print(person)
+# print(person)
 
 # Exercício 11
 # Realizar a contagem de quantas vezes cada elemento aparece em uma
@@ -207,5 +207,113 @@ for item in array:
     else:
         dict[item] = 1
 
-for key, valor in dict.items():
-    print(f"{key} : {valor}")
+# for key, valor in dict.items():
+#     print(f"{key} : {valor}")
+
+
+# salary = int(input("Qual o seu salário? "))
+salary = 1
+
+position = ""
+if salary <= 2000:
+    position = "estagiário"
+elif 2000 < salary <= 5800:
+    position = "júnior"
+elif 5800 < salary <= 7500:
+    position = "pleno"
+elif 7500 < salary <= 10500:
+    position = "senior"
+else:
+    position = "líder"
+
+# print(position)
+
+
+restaurants = [
+    {"name": "Restaurante A", "nota": 4.5},
+    {"name": "Restaurante B", "nota": 3.0},
+    {"name": "Restaurante C", "nota": 4.2},
+    {"name": "Restaurante D", "nota": 2.3},
+]
+
+print(restaurants[0])
+
+# criando uma lista com o if
+min_rating = 3.0
+filtered_restaurants = [
+    restaurant for restaurant in restaurants if restaurant["nota"] > min_rating
+]
+
+# criando uma lista só com os nomes
+# filtered_restaurants = [
+#     restaurant["name"]
+#     for restaurant in restaurants
+#     if restaurant["nota"] > min_rating
+# ]
+# print(filtered_restaurants)
+
+
+# for index in range(5):
+#     print(index)
+
+names_list = ["Duda", "Rafa", "Cris", "Yuri"]
+new_names_list = [name for name in names_list if "a" in name]
+
+# Aqui o for percorre cada nome em "names_list", verifica se existe a
+# letra "a" nele,
+# o adiciona à variável "name", e então gera nossa nova lista "new_names_list"
+print(new_names_list)
+
+quadrados = [x * x for x in range(11)]
+print(quadrados)
+
+languages = ["Python", "Java", "JavaScript"]
+
+enumerate_prime = enumerate(languages)
+
+# converte um objeto enumerate em uma lista
+# print(list(enumerate_prime))
+
+languages = ["Python", "Java", "JavaScript"]
+
+# for index, language in enumerate(["Python", "Java"]):
+#     print(f"{index} - {language}")
+
+# n = 10
+# last, next = 0, 1
+# while last < n:
+#     print(last)
+#     last, next = next, last + next
+
+# Exercício 12
+# O Fatorial de um número inteiro é representado pela multiplicação de todos
+#  os números predecessores maiores que 0. Por exemplo, o fatorial de 5 é 120
+#  pois 5! = 1 * 2 * 3 * 4 * 5. Escreva um código que calcule o fatorial de
+# um número inteiro.
+
+fatorial = 1
+numero = 5
+while numero > 0:
+    fatorial = fatorial * numero
+    numero = numero - 1
+print(fatorial)
+
+# Exercício 13
+# Um sistema de avaliações registra valores de 0 a 10 para cada avaliação.
+# Após algumas mudanças estes valores precisam ser ajustados para avaliações
+# de 0 a 100. Dado uma sequência de avaliações ratings = [6, 8, 5, 9, 10].
+# Escreva um código capaz de gerar as avaliações após a mudança. Neste caso o
+# resultado deveria ser [60, 80, 50, 90, 100].
+
+ratings = [6, 8, 5, 9, 10]
+adjusted_ratings = [rating * 10 for rating in ratings]
+
+print(adjusted_ratings)
+
+# Exercício 14
+# Percorra a lista do exercício 13 e imprima “Múltiplo de 3” se o elemento
+# for divisível por 3.
+
+for rating in adjusted_ratings:
+    if rating % 3 == 0:
+        print(f"{rating} é multiplo de 3")
