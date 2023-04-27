cf5fbf32f01b2c91a4f8a05bd12866a83886c2ef
Thierry <varelathierry@gmail.com>
2023-04-27 17:31:14 -0300

    feat: exercises day1

--- ciencia-da-computacao/bloco-28-introducao-a-python/dia-01-aprendendo-python/conteudo.py
+++ ciencia-da-computacao/bloco-28-introducao-a-python/dia-01-aprendendo-python/conteudo.py
@@ -317,3 +317,33 @@ print(adjusted_ratings)
 for rating in adjusted_ratings:
     if rating % 3 == 0:
         print(f"{rating} é multiplo de 3")
+
+
+def sum(num1, num2):
+    return num1 + num2
+
+
+# posicionada
+print(sum(12, 1))
+
+# nomeada
+print(sum(num1=14, num2=1))
+
+
+def concat(*strings):
+    # Equivalente a um ", ".join(strings), que concatena os
+    # elementos de um iterável em uma string utilizando um separador
+    # Nesse caso a string resultante estaria separada por vírgula
+    final_string = ""
+    for string in strings:
+        final_string += string
+        if not string == strings[-1]:
+            final_string += ", "
+    return final_string
+
+
+# pode ser chamado com 2 parâmetros
+concat("Carlos", "Cristina")  # saída: "Carlos, Cristina"
+
+# pode ser chamado com um número n de parâmetros
+concat("Carlos", "Cristina", "Maria")  # saída: "Carlos, Cristina, Maria"
